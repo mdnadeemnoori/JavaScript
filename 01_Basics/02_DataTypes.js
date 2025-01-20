e07d2cf364aa07f030f86e855684072abea34df9
MD NADEEM NOORI <47346448+mdnadeemnoori@users.noreply.github.com>
2025-01-20 12:14:27 +0000

    data_type_learned

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+// alert (3+3) // we are using node js not browser so it will not treate like browser console
+
+console.log("Nadeem Noori")
+// TO DISCUSS DATA TYPES
+let Name = "Nadeem"
+let age = 26
+let isEligable = true
+console.log(typeof (age, isEligable, Name))
+
+
+//number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+// null => standalone value
+// undifined =>
+// symbol => unique
+
+console.log(typeof null);
+console.log(typeof undefined);
