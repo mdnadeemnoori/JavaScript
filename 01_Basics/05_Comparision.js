a52518e15bd95c39d1bd5c31b0ff7660ab874d29
MD NADEEM NOORI <47346448+mdnadeemnoori@users.noreply.github.com>
2025-01-21 09:22:15 +0000

    comparision done

---
+++ 01_Basics/05_Comparision.js
@@ -0,0 +1,14 @@
+console.log(2>3)
+console.log(3<=3)
+console.log(2!=1)
+console.log(2!=2)
+// comparision of null values with zero
+console.log("*************************")
+console.log(null>0)
+console.log(null>=0) //comparision
+console.log(null==0)//equality check
+// comparision of undefined values with zero
+console.log("*************************")
+console.log(undefined>0)
+console.log(undefined>=0)
+console.log(undefined==0)
