3e311787edb0b887c52f0dea2682dc7b5168fd07
MD NADEEM NOORI <47346448+mdnadeemnoori@users.noreply.github.com>
2025-01-21 07:27:30 +0000

    operator operation done

---
+++ 01_Basics/04_Operation_on_operator.js
@@ -0,0 +1,12 @@
+let operand1 = 30
+//postfix operation
+const op1 = operand1++
+console.log(`operand1:${operand1}, op1:${op1}`);
+//infix or prefix operation
+let operand2 = 35
+const op2 = ++operand2
+console.log(`operand2:${operand2}, op2:${op2}`);
+//unary negation operator
+let operand3 = 36
+const op3 = -operand3
+console.log(op3)
